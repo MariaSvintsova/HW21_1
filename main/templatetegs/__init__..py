b9359054b99ed26d59ba885bb543ffbbdeb91a7d
MariaSvintsova <m.svintsova@it-park.tech>
2024-04-04 22:27:47 +0300

    20_2

---
+++ main/templatetegs/__init__..py
@@ -0,0 +1,1 @@
+from .mediapath_tag import *
--- main/templatetegs/mediapath_tag.py
+++ main/templatetegs/mediapath_tag.py
@@ -1,9 +1,9 @@
-# custom_filters.py
 from django import template
 from django.conf import settings
 
 register = template.Library()
 
-@register.filter
+
+@register.simple_tag(name='mediapath')
 def mediapath(image_path):
     return f"{settings.MEDIA_URL}{image_path}"
